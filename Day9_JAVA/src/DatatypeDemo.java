18edca33490a71e667d7f201048e4f345564112d
Nilotpal <Nilotpal@DESKTOP-9CN1MJ5>
2022-03-14 16:23:41 +0530

    Datatype demo program

---
+++ Day9_JAVA/src/DatatypeDemo.java
@@ -0,0 +1,50 @@
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b=false;
+		System.out.println(b);
+		
+	//	boolean b1=1;        // error
+	//	System.out.println(b1);
+		
+		byte bi=55;
+		System.out.println(bi);
+		
+	//	byte bi1=220;      // error in datatype range
+	//	System.out.println(bi1);
+		
+		char c='n';
+		System.out.println(c);
+		
+	//	char c='nil';    //only one character allowed
+	// System.out.println(c);
+		
+		short s=29765;
+		System.out.println(s);
+		
+	//	short s1=132644;
+	//	System.out.println(s1);   // error in datatype range
+		
+		int num=25089922;
+		System.out.println(num);
+		
+	//	int num1=25089922;
+	//	System.out.println(num1); // error in datatype range
+		
+		long lnum=6598955214l;
+		System.out.println(lnum);
+		
+		float fnum=12.5f;
+		System.out.println(fnum);
+		
+		float fnum1=444444.22222f;
+		System.out.println(fnum1);
+		
+		double dnum=445454.87968;
+		System.out.println(dnum);
+		
+	}
+
+}
